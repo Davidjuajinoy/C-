9d395915fbe85113f89c9f9b828d9bac12aa7404
David Juajinoy <davidhernandezjuajinoy@gmail.com>
2021-04-05 14:05:27 -0500

    API[OPTION]

---
+++ ApiRest/TheCodeCamp/Controllers/OperationsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class OperationsController : ApiController
+    {
+        [HttpOptions]
+        [Route("api/refreshconfig")]
+        // se consume por medio de la opcion option en postman.
+        public IHttpActionResult RefreshAppSettings()
+        {
+            try
+            {
+                ConfigurationManager.RefreshSection("AppSettings");
+                return Ok();
+            }
+            catch (System.Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
